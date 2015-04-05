6d350b24ef85112e06662e0db3cf61eeaee6134e
AlexSt2501 <alexst2501@gmail.com>
2015-04-05 12:15:32 +0300

    Install Migrations Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Rating\MainBundle\RatingMainBundle(),
             new Rating\DashboardBundle\RatingDashboardBundle(),
             new Rating\UserBundle\RatingUserBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
