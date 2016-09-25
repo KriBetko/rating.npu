2f99648913334c8d2f676990d2720acefd57f184
kribetko <kribetko@gmail.com>
2016-09-25 15:28:39 +0300

    Fix references

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,12 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Rating\AdminBundle\RatingAdminBundle(),
+            new Rating\MainBundle\RatingMainBundle(),
+            new Rating\ProfileBundle\RatingProfileBundle(),
+            new Rating\UserBundle\RatingUserBundle(),
+            new System\AppBundle\SystemAppBundle(),
+            new Rating\SubdivisionBundle\RatingSubdivisionBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
