e4854089326efbfc68c9433e75ea896d477532a0
kribetko <kribetko@gmail.com>
2016-10-01 15:54:10 +0300

    Cleanup

--- src/System/AppBundle/Controller/DefaultController.php
+++ src/System/AppBundle/Controller/DefaultController.php
@@ -2,15 +2,15 @@
 
 namespace System\AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/hello/{name}")
-     * @Template()
+     * @param $name
+     * @return array
      */
     public function indexAction($name)
     {
