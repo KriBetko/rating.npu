88f127f447456c033e40f9d779ce7217b75e8724
kribetko <kribetko@gmail.com>
2016-10-07 15:18:47 +0300

    Move GoogleAuth tokens to parameters.yml

--- src/UserBundle/Security/GoogleService.php
+++ src/UserBundle/Security/GoogleService.php
@@ -20,12 +20,10 @@ class GoogleService
 
     public function init()
     {
-        // 704118661131-33bh0uukfro8o3ne5c727cd5bo9nljln.apps.googleusercontent.com
-        // gUVeePzKyw6ZSFd6tygTAyX3
-        $client = new \Google_Client( //TODO Hide id and secret
+        $client = new \Google_Client(
             [
-                'client_id' => "375709262973-g7q7p85bh2qk0ieddmafm665sqf71rek.apps.googleusercontent.com",//$this->container->getParameter('google_client_id'),
-                'client_secret' => "OYvnMHoW712hJrRS3fGV_gYy"//$this->container->getParameter('google_client_secret'),
+                'client_id' => $this->container->getParameter('google_client_id'),
+                'client_secret' => $this->container->getParameter('google_client_secret'),
             ]
         );
         $client->setRedirectUri($this->generateRedirectUrl());
